af0f49216cfd6fb18cbc780f968da06c3dd997dc
めいびぃ <162483806+Maybe515@users.noreply.github.com>
2025-05-16 17:01:27 +0900

    Add files via upload

---
+++ WifiMonitor/Form2.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WifiMonitor
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.表示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.文字サイズToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.小ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.標準ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.大ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.特大ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 377);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 18, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(782, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(57, 20);
+            this.toolStripStatusLabel1.Text = "AP数：";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(84, 20);
+            this.toolStripStatusLabel2.Text = "LastRead：";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.表示ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(782, 30);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(82, 24);
+            this.ファイルToolStripMenuItem.Text = "ファイル(F)";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.終了ToolStripMenuItem.Text = "終了(X)";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // 表示ToolStripMenuItem
+            // 
+            this.表示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文字サイズToolStripMenuItem});
+            this.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem";
+            this.表示ToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.表示ToolStripMenuItem.Text = "表示(V)";
+            // 
+            // 文字サイズToolStripMenuItem
+            // 
+            this.文字サイズToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.小ToolStripMenuItem,
+            this.標準ToolStripMenuItem,
+            this.大ToolStripMenuItem,
+            this.特大ToolStripMenuItem});
+            this.文字サイズToolStripMenuItem.Name = "文字サイズToolStripMenuItem";
+            this.文字サイズToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.文字サイズToolStripMenuItem.Text = "フォントサイズ";
+            // 
+            // 小ToolStripMenuItem
+            // 
+            this.小ToolStripMenuItem.Name = "小ToolStripMenuItem";
+            this.小ToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.小ToolStripMenuItem.Text = "小";
+            // 
+            // 標準ToolStripMenuItem
+            // 
+            this.標準ToolStripMenuItem.Name = "標準ToolStripMenuItem";
+            this.標準ToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.標準ToolStripMenuItem.Text = "標準";
+            // 
+            // 大ToolStripMenuItem
+            // 
+            this.大ToolStripMenuItem.Name = "大ToolStripMenuItem";
+            this.大ToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.大ToolStripMenuItem.Text = "大";
+            // 
+            // 特大ToolStripMenuItem
+            // 
+            this.特大ToolStripMenuItem.Name = "特大ToolStripMenuItem";
+            this.特大ToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.特大ToolStripMenuItem.Text = "特大";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 33);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(782, 341);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "SSID";
+            this.columnHeader1.Width = 107;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Channel";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 112;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Signal";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 94;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "認証";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 92;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "MACアドレス";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 152;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "暗号化";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 108;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "無線タイプ";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader7.Width = 136;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "インターネットの種類";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader8.Width = 195;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 403);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("メイリオ", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form2";
+            this.Text = "WifiMonitor";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 表示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripMenuItem 文字サイズToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 小ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 標準ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 大ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 特大ToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
+
---
+++ WifiMonitor/Form2.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WifiMonitor
+{
+    public partial class Form2 : Form
+    {
+        const string _netsh = @"C:\Windows\System32\netsh.exe";
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            InitializeForm();
+            LoopDo();
+        }
+        /// <summary>
+        /// フォームを初期化する
+        /// </summary>
+        private void InitializeForm()
+        {
+            
+            // string IcoPath = @"";
+            // this.Icon = new System.Drawing.Icon(IcoPath);
+            // this.AutoScroll = true;
+            listView1.Items.Clear();    // リストビュー初期化
+
+        }
+        /// <summary>
+        /// ループ処理
+        /// </summary>
+        public void LoopDo()
+        {
+            ProcessStartInfo ps = new ProcessStartInfo(_netsh);
+            ps.Arguments = " wl show networks mode=bssid";
+            ps.CreateNoWindow = true;
+            ps.UseShellExecute = false;
+            ps.RedirectStandardOutput = true;
+            ps.RedirectStandardError = true;
+
+            bool Loop = true;
+            SynchronizationContext mainContext = SynchronizationContext.Current;
+
+            // 非同期処理
+            Task.Run(async () =>
+            {
+                while (Loop)
+                {
+                    try
+                    {
+                        await Task.Delay(1000);     // 1秒間隔で情報を取得
+
+                        // ラベルに表示する情報を取得する
+                        int intAP = GetAP(ps);
+                        string strDate = GetNowTime();
+
+                        AddLabel(intAP, strDate);
+
+                        // リストビューに表示する情報を取得する
+                        string[] result = GetWlanInfo(ps,intAP);
+
+                        //mainContext.Post(x => AddListView(ssid, cha, sig, sec, mac, ben, enc, wlan, netwk), null);
+                        mainContext.Post(x => AddListView(result), null);
+                    }
+                    catch (Exception) { }
+                }
+            });
+        }
+        /// <summary>
+        /// アクセスポイント数を取得する
+        /// </summary>
+        private int GetAP(ProcessStartInfo ps)
+        {
+            int result = new int();
+         
+            try
+            {
+                Process p = Process.Start(ps);
+                string output = p.StandardOutput.ReadToEnd();
+                output = FormatOutput(output);
+
+                string[] ss = output.Split('\n');
+
+                string ptn = "現在";
+                string[] del = new string[] { ptn, "の" };
+
+                foreach (string s in ss)
+                {
+                    if (Regex.IsMatch(s, ptn))
+                    {
+                        string[] splitCodes = s.Split(del, StringSplitOptions.None);
+                        result = int.Parse(splitCodes[1]);
+                    }
+                }
+                p.Close();
+                p.Dispose();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// 現在日時を取得する
+        /// </summary>
+        private string GetNowTime()
+        {
+            DateTime Now = DateTime.Now;
+            string result = Now.ToString("yyyy/MM/dd HH:mm:ss");
+            return result;
+        }
+        /// <summary>
+        /// 取得した情報をリストビューに追加する
+        /// </summary>
+        private void AddListView(string[] result)
+        {
+            
+            List<ListViewItem> listItem = new List<ListViewItem>();
+            try
+            { 
+                for (int i = 0; i < result.Length; i++)
+                { 
+                    string[] arrInfo = result[i].Split(',');
+                    listItem.Add(new ListViewItem(arrInfo));
+                }
+                listView1.Items.Clear();
+                listView1.Items.AddRange(listItem.ToArray());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// 取得した情報をラベルに表示する
+        /// </summary>
+        /// <param name="intAP"></param>
+        /// <param name="strDate"></param>
+        private void AddLabel(int intAP, string strDate)
+        {
+            toolStripStatusLabel1.Text = "AP数：" + intAP;
+            toolStripStatusLabel2.Text = "LastRead：" + strDate;
+        }
+        /// <summary>
+        /// WLAN情報を取得する
+        /// </summary>
+        /// <param name="ps"></param>
+        /// <param name="intAP"></param>
+        /// <returns></returns>
+        private string[] GetWlanInfo(ProcessStartInfo ps, int intAP)
+        {
+            // 初期化
+            int i = 0;
+            int idx = 0;
+            string str = "";
+
+            string ssidNum = "";
+            string ssid = "";
+            string netwk = "";
+            string sec = "";
+            string enc = "";
+            string mac = "";
+            string sig = "";
+            string wlty = "";
+            string cha = "";
+
+            string[] ptn = { "SSID[1-100]:", "ネットワークの種類:", "認証:", "暗号化:", "BSSID1:", "シグナル:", "無線タイプ:", "チャネル:" };
+
+            try
+            {
+                // コマンドを実行し、取得した出力結果を整形する
+                Process p = Process.Start(ps);
+                string output = p.StandardOutput.ReadToEnd();
+                output = FormatOutput(output);
+
+                string[] ss = output.Split('\n');
+                string[] result = new string[intAP];
+
+                foreach (string s in ss)
+                {
+                    if (Regex.IsMatch(s, ptn[i]))
+                    {
+                        if (i == 0)   // SSID名
+                        {
+                            string[] splitCodes = s.Split(':');
+                            ssidNum = splitCodes[0];
+                            ssid = splitCodes[1];
+                            i++;
+                        }
+                        else
+                        {
+                            str = s.Replace(ptn[i], "");
+
+                            switch (i)
+                            {
+                                case 1:     // ネットワークの種類
+                                    netwk = str;
+                                    break;
+
+                                case 2:     // 認証
+                                    sec = str;
+                                    break;
+
+                                case 3:     // 暗号化
+                                    enc = str;
+                                    break;
+
+                                case 4:     // MACアドレス
+                                    mac = str;
+                                    break;
+
+                                case 5:     // シグナル
+                                    sig = str;
+                                    break;
+
+                                case 6:     // 無線タイプ
+                                    wlty = str;
+                                    break;
+
+                                case 7:     // チャンネル
+                                    cha = str;
+                                    break;
+                            }
+                            i++;
+                        }
+                    }
+                    if (i == ptn.Length)
+                    {
+                        if (ssid != null && cha != null && sig != null && sec != null && mac != null && enc != null && wlty != null && netwk != null)
+                        {
+                            string[] arr = { ssid, cha, sig, sec, mac, enc, wlty, netwk };
+                            result[idx] = string.Join(",", arr);
+                            idx++;
+
+                            i = 0;
+                        }
+                    }
+                }   
+                p.Close();
+                p.Dispose();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        /// <summary>
+        /// [終了(X)]を押下した時の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// プロセスの出力の整形
+        /// </summary>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        internal static string FormatOutput(string output)
+        {
+            string result = "";
+            try
+            {
+                result = output.Replace("\r\r\n", "\n"); // 余分な改行や空白の除去
+                result = result.Replace("\r", "");
+                result = result.TrimEnd(' ');
+                result = result.Replace(" ", "");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
